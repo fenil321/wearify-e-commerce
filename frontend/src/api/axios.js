039577108049880df0a46cd443cb34ab5ee7e17f
fenil321 <fparmar163@gmail.com>
2026-04-25 16:59:49 +0530

    initial commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: API_BASE_URL,
   withCredentials: true,
 });
 
